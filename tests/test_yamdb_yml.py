b1fe9f68581fd093cfcdcf8029d7aad7ea166027
DonFortes <53881876+DonFortes@users.noreply.github.com>
2021-02-22 15:02:55 +0300

    Initial commit

---
+++ tests/test_yamdb_yml.py
@@ -0,0 +1,21 @@
+import os
+
+from django.conf import settings
+
+
+class TestWorkflow:
+
+    def test_workflow(self):
+        try:
+            with open(f'{os.path.join(settings.BASE_DIR, "yamdb_workflow.yaml")}', 'r') as f:
+                yamdb = f.read()
+        except FileNotFoundError:
+            assert False, 'Проверьте, что добавили файл yamdb_workflow.yaml в корневой каталог для проверки'
+
+        assert 'on: [push]' in yamdb, 'Проверьте, что добавили действие при пуше в файл yamdb_workflow.yaml'
+        assert 'pytest' in yamdb, 'Проверьте, что добавили pytest в файл yamdb_workflow.yaml'
+        assert 'appleboy/ssh-action' in yamdb, 'Проверьте, что добавили деплой в файл yamdb_workflow.yaml'
+        assert 'appleboy/telegram-action' in yamdb, (
+            'Проверьте, что добавили доставку отправку telegram сообщения '
+            'в файл yamdb_workflow.yaml'
+        )
