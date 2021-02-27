7ba8338af4f4ee2902b62f0f011b45ad897dac2f
Павел <paul-36@mail.ru>
2021-02-27 21:15:30 +0300

    final

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@ python3 -m pip install --upgrade pip
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic  --noinput
+gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
+
+
 exec "$@"
